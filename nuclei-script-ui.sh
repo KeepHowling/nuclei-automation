64d3a33df85a92041dd98783767a467d5a1e8829
Marina J Villalta Cerezo <marinavcsg@gmail.com>
2022-06-09 01:16:33 +0200

    Update nuclei-script-ui.sh

--- nuclei-script-ui.sh
+++ nuclei-script-ui.sh
@@ -2,39 +2,39 @@
 echo -e "\n\033[36m>>>\033[35m Nuclei-automation script \033[m\n";
 nuclei --update-templates --silent
 echo -e "\033[33m[!] Enter domain names seperated by 'space' \033[m" 
-read -p " --> " input
+read -p " ----->>>>>>>> " input
 for i in ${input[@]}
 do
 
-echo -e "\n\033[36m>>>\033[35m Scan started for ~/Documents/Nuclei-automation/$i \033[m\n"
+echo -e "\n\033[36m>>>\033[35m Scan started for ./$i \033[m\n"
  
-mkdir ~/Documents/Nuclei-automation/$i  
+mkdir ./$i  
 
 echo -e "\n\033[33m[!] Subfinder and Assetfinder Running..................\033[m"
-subfinder -d $i -o ~/Documents/Nuclei-automation/$i/subdomains
-assetfinder -subs-only $i | tee -a ~/Documents/Nuclei-automation/$i/subdomains
+subfinder -d $i -o ./$i/subdomains
+assetfinder -subs-only $i | tee -a ./$i/subdomains
 echo -e "\n\033[36m Total no. of unique subdomains found -->>\033[m"
-sort -u ~/Documents/Nuclei-automation/$i/subdomains -o ~/Documents/Nuclei-automation/$i/subdomains
-cat ~/Documents/Nuclei-automation/$i/subdomains | wc -l
-echo -e "\n\033[33m[!] Subdomains saved at ~/Documents/Nuclei-automation/$i/subdomains\033[m"
+sort -u ./$i/subdomains -o ./$i/subdomains
+cat ./$i/subdomains | wc -l
+echo -e "\n\033[33m[!] Subdomains saved at ./$i/subdomains\033[m"
 
 echo -e "\n\033[36m>>>\033[35m Scan for CVES started\033[m\n"
-nuclei -l ~/Documents/Nuclei-automation/$i/subdomains -t cves/ -o ~/Documents/Nuclei-automation/$i/cves
+nuclei -l ./$i/subdomains -t cves/ -o ./$i/cves
 echo -e "\n\033[33m[!] Scan for CVES completed\033[m"
 echo -e "\n\033[36m>>>\033[35m Scan for default-login started\033[m\n"
-nuclei -l ~/Documents/Nuclei-automation/$i/subdomains -t default-logins/ -o ~/Documents/Nuclei-automation/$i/default-logins
+nuclei -l ./$i/subdomains -t default-logins/ -o ./$i/default-logins
 echo -e "\n\033[33m[!] Scan for default-login completed\033[m"
 echo -e "\n\033[36m>>>\033[35m Scan for exposures started\033[m\n"
-nuclei -l ~/Documents/Nuclei-automation/$i/subdomains -t exposures/ -o ~/Documents/Nuclei-automation/$i/exposures
+nuclei -l ./$i/subdomains -t exposures/ -o ./$i/exposures
 echo -e "\n\033[33m[!] Scan for exposures completed\033[m"
 echo -e "\n\033[36m>>>\033[35m Scan for misconfigurations started\033[m\n"
-nuclei -l ~/Documents/Nuclei-automation/$i/subdomains -t misconfiguration/ -o ~/Documents/Nuclei-automation/$i/misconfigurations
+nuclei -l ./$i/subdomains -t misconfiguration/ -o ./$i/misconfigurations
 echo -e "\n\033[33m[!] Scan for misconfigurations completed\033[m"
 echo -e "\n\033[36m>>>\033[35m Scan for takeovers started\033[m\n"
-nuclei -l ~/Documents/Nuclei-automation/$i/subdomains -t takeovers/ -o ~/Documents/Nuclei-automation/$i/takeovers
+nuclei -l ./$i/subdomains -t takeovers/ -o ./$i/takeovers
 echo -e "\n\033[33m[!] Scan for takeovers completed\033[m"
 echo -e "\n\033[36m>>>\033[35m Scan for vulnerabilities started\033[m\n"
-nuclei -l ~/Documents/Nuclei-automation/$i/subdomains -t vulnerabilities/ -o ~/Documents/Nuclei-automation/$i/vulnerabilities
+nuclei -l ./$i/subdomains -t vulnerabilities/ -o ./$i/vulnerabilities
 echo -e "\n\033[33m[!] Scan for vulnerabilities completed\033[m"
 echo -e "\n\033[36m>>>\033[35m
 '
